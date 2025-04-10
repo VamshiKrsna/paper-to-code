402e055ebe1d626f4123f991cfdce1ad4d8360b9
VamshiKrsna <107762231+VamshiKrsna@users.noreply.github.com>
2025-04-10 21:24:03 +0530

    completed gpt implementation

--- gpt/src/tinygpt.py
+++ gpt/src/tinygpt.py
@@ -1,6 +1,8 @@
+import math
 import torch.nn as nn 
 import torch.nn.functional as F
 import numpy as np
+import torch
 
 class SelfAttentionHead(nn.Module):
     """
@@ -76,36 +78,232 @@ class SelfAttentionHead(nn.Module):
 
         return out
 
-class MultiHeadAttention(nn.Module):
+class MultiHeadSelfAttention(nn.Module):
     """
     Multiple heads of self-attention in parallel.
     """
-    def __init__():
-        pass 
-
-    def forward():
-        pass  
+    def __init__(self, n_embed, n_head):
+        """
+        args:
+            - n_embed : dimensionality of the input sequence.
+            - n_head : number of heads of self-attention.
+        returns:
+            - Nothing, but initializes the heads of self-attention.
+        """
+        super().__init__()
+        head_size = n_embed // n_head
+        self.heads = nn.ModuleList(
+            [SelfAttentionHead(n_embed, head_size = n_embed // n_head) for i in range(n_head)]
+        )
+        self.proj = nn.Linear(n_embed, n_embed)
+        
+    def forward(self, x):
+        """
+        args:
+            - x : input sequence of shape (batch_size:128, seq_len, n_embed)
+        returns:
+            - self.proj(out) : weighted sum of value vectors using
+                    normalized attention weights.
+        """
+        out = torch.cat(
+            [
+                h(x) for h in self.heads
+            ], dim = -1
+        )
+        return self.proj(out)
+          
 
 class TransformerBlock(nn.Module):
     """
     Uniting everything at a place
     """
-    def __init__():
-        pass 
+    def __init__(self, n_embed, n_head):
+        """
+        args: 
+            - n_embed : dimensionality of the input sequence.
+            - n_head : number of heads of self-attention.
+        returns:
+            Nothing.
+        """
+        super().__init__()
+        self.sa = MultiHeadSelfAttention(
+            n_embed, n_head 
+        )
+        self.ff = nn.Sequential(
+            nn.Linear(
+                n_embed, n_embed * 4
+            ),
+            nn.ReLU(),
+            nn.Linear(n_embed * 4, n_embed),
+        )
+        self.ln1 = nn.LayerNorm(n_embed)
+        self.ln2 = nn.LayerNorm(n_embed)
 
-    def forward():
-        pass  
+    def forward(self, x):
+        """
+        args:
+            - x : input sequence of shape (batch_size:128, seq_len, n_embed)
+        returns:
+            - x : output sequence of shape (batch_size:128, seq_len, n_embed) 
+                  after passing through the self-attention and feed-forward
+                  layers.
+        """
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ff(self.ln2(x))
+        return x 
 
-class GPT(nn.module):
+class GPT(nn.Module):
     """
     The GPT model.
     """
-    def __init__():
-        pass 
+    def __init__(
+        self, 
+        vocab_size,
+        block_size, 
+        n_embed = 128,
+        n_head = 4,
+        n_layer = 2
+    ):
+        super().__init__() 
+        self.token_embed = nn.Embedding(
+            vocab_size,
+            n_embed
+        )
+        self.pos_embed = nn.Embedding(
+            block_size,
+            n_embed
+        )
+        self.blocks = nn.Sequential(
+            *[
+                TransformerBlock(n_embed, n_head) for i in range(n_layer)
+            ]
+        )
+        self.ln_f = nn.LayerNorm(
+            n_embed
+        )
+        self.head = nn.Linear(
+            n_embed,
+            vocab_size
+        )
 
-    def forward():
-        pass  
+    def forward(self,x):
+        # B: Batch Size, T: No. Of Tokens/ Sequence Length, C: Number of Embeddings
+        B, T = x.shape 
+        token_emb = self.token_embed(x) # (B,T,C)
+        pos = torch.arange(
+            T, device = x.device
+        )
+        pos_emb = self.pos_embed(pos)[None, : , :] # (1,T,C)
+        x = token_emb + pos_emb 
+        x = self.blocks(x) 
+        x = self.ln_f(x) 
+        logits = self.head(x) 
+        return logits
+
+class Helpers():
+    def encode(self,s): 
+        return [stoi[c] for c in s]
+
+    def decode(self,l): 
+        return ''.join([itos[i] for i in l])
+
+    def get_batch(self,split, batch_size=64):
+        data_ = train_data if split == "train" else val_data
+        ix = torch.randint(len(data_) - block_size, (batch_size,))
+        x = torch.stack([data_[i:i+block_size] for i in ix])
+        y = torch.stack([data_[i+1:i+block_size+1] for i in ix])
+        return x, y
 
 
 if __name__ == "__main__":
-    print("Hello there!") 
+    # Pretraining and testing out : 
+    # Data prep : 
+    with open("../data/sherlock_holmes.txt", "r", encoding= "cp1252") as f:
+        text = f.read()
+    print(f"Length of text: {len(text)} characters")
+
+    # tokenization : 
+    chars = sorted(list(set(text)))
+    vocab_size = len(chars)
+    print(f"Number of unique characters: {vocab_size}")
+
+    # char to int and vice versa : 
+    stoi = {
+        ch:i for i,ch in enumerate(chars)
+    }
+    itos = {
+        i:ch for ch,i in stoi.items()
+    }
+
+    data = torch.tensor(Helpers().encode(text), dtype=torch.long)
+
+    split_idx = int(0.9 * len(data))
+    train_data = data[:split_idx]
+    val_data = data[split_idx:]
+
+    block_size = 128  # context length
+
+    # Training : 
+    model = GPT(
+        vocab_size=vocab_size,
+        block_size=block_size,
+    )
+    optimizer = torch.optim.AdamW(
+        model.parameters(),
+        lr = 1e-3
+    )
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print(device)
+    model.to(device=device)
+
+    def estimate_loss():
+        model.eval()
+        losses = {'train': 0, 'val': 0}
+        for split in ['train', 'val']:
+            x, y = Helpers().get_batch(split)
+            x, y = x.to(device), y.to(device)
+            logits = model(x)
+            B, T, C = logits.shape
+            loss = F.cross_entropy(logits.view(B*T, C), y.view(B*T))
+            losses[split] += loss.item()
+        model.train()
+        return losses
+
+    for step in range(30000):
+        xb, yb = Helpers().get_batch("train")
+        xb, yb = xb.to(device), yb.to(device)
+
+        logits = model(xb)
+        B, T, C = logits.shape
+        loss = F.cross_entropy(logits.view(B*T, C), yb.view(B*T))
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        if step % 100 == 0:
+            losses = estimate_loss()
+            print(f"Step {step} | Train loss: {losses['train']:.4f} | Val loss: {losses['val']:.4f}")
+
+    # Eval, Test : 
+    perplexity = math.exp(losses["val"])
+    print(f"Perplexity : {perplexity:.2f}")
+
+    # Text Generation : 
+    def generate(
+        model, 
+        start, 
+        max_new_tokens = 30
+    ):
+        model.eval()
+        context = torch.tensor(Helpers().encode(start), dtype=torch.long)[None, :].to(device)
+        for i in range(max_new_tokens):
+            x_cond = context[:,-block_size:]
+            logits = model(x_cond)
+            logits = logits[:,-1,:]
+            probs = F.softmax(logits,dim = -1)
+            next_id = torch.multinomial(probs, num_samples=1)
+            context = torch.cat([context, next_id],dim = 1)
+        return Helpers().decode(context[0].tolist())
+
+    print(generate(model, "And brought miss hunter from "))
